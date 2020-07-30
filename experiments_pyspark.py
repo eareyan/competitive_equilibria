0c255629483054dcb829549d4b6911ecdd5d2a17
eareyan <enrique3@gmail.com>
2020-07-30 13:13:06 -0400

    working experiments on google's dataproc

--- experiments_pyspark.py
+++ experiments_pyspark.py
@@ -1,9 +1,11 @@
-from pyspark.sql import SparkSession
-from pyspark.sql.functions import udf
-from pyspark.sql.types import IntegerType, StructType, StructField
 from market import Market
 from market_constituents import Good
 from bidders import SingleMinded
+from pyspark import SparkContext
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf
+from pyspark.sql.types import IntegerType, StructType, StructField
+import sys
 
 
 def check_ce(sm_market):
@@ -18,13 +20,14 @@ def check_ce(sm_market):
     return 1 if pricing_result['status'] == 'Optimal' else 0, 1 if non_linear_pricing_result['status'] == 'Optimal' else 0
 
 
-def checking_clearing_prices(*args):
+def does_market_clear(*args):
     """ Receives a market in the form of a row of a .csv file and returns a tuple
     (clears_linear, clears_non_linear) where each member of the tuple is 0/1 indicating the event. """
     INDEX_INDEX = 0
     NUM_BIDDERS_INDEX = 1
     NUM_GOODS_INDEX = 2
 
+    # At each 1000 market, print some debug info. This only makes logical sense when the processing is sequential.
     if int(args[INDEX_INDEX]) % 1000 == 0:
         print(f"market #{args[INDEX_INDEX]}")
 
@@ -57,25 +60,57 @@ def checking_clearing_prices(*args):
     return linear_clear, non_linear_clear
 
 
-# Run the pyspark experiment with the following parameters.
-total = 6
-sm_market_input_gz_loc = f"all_sm_markets/values_1_to_10/sm_market_{total}_values_1_to_10.gz"
-sm_market_output_parquet_loc = f"experiments_results/markets_{total}.parquet"
+def run_experiment(total, input_path, output_path):
+    # Run the pyspark experiment with the following parameters.
+    sm_market_input_gz_loc = f"{input_path}sm_market_{total}_values_1_to_10.gz"
+    sm_market_output_parquet_loc = f"{output_path}markets_{total}.parquet"
 
-spark = SparkSession.builder.master("local").appName("sm_market_clearing").getOrCreate()
-df = spark.read.csv(sm_market_input_gz_loc, header=True)
+    # Create the spark context and then the spark session.
+    sc = SparkContext()
+    spark = SparkSession(sc)
 
-# Create a udf to compute clearing prices.
-schema = StructType([
-    StructField("linear", IntegerType(), False),
-    StructField("non_linear", IntegerType(), False)
-])
-# Register the udf function
-my_udf = udf(checking_clearing_prices, schema)
-cols = ['index', 'num_bidders', 'num_goods'] + [f'col_{k}' for k in range(0, 2 * (total - 1))]
-df = df.withColumn('clearing', my_udf(*cols))
-new_cols = cols + ['clearing.linear', 'clearing.non_linear']
-df = df.select(*new_cols)
-df.write.mode('overwrite').parquet(sm_market_output_parquet_loc)
-df.printSchema()
-# df.show(n=1000)
+    # Read in the input markets for the experiment.
+    df = spark.read.csv(sm_market_input_gz_loc, header=True)
+    print(f"default number of partitions = {df.rdd.getNumPartitions()}")
+
+    # TODO the number of partitions are fixed. How to make this number dynamic based on the input data?
+    df = df.repartition(100)
+    print(f"new number of partitions = {df.rdd.getNumPartitions()}")
+
+    # Create and register a udf to compute clearing prices.
+    schema = StructType([
+        StructField("linear", IntegerType(), False),
+        StructField("non_linear", IntegerType(), False)
+    ])
+    my_udf = udf(does_market_clear, schema)
+
+    # Apply the UDF function.
+    cols = ['index', 'num_bidders', 'num_goods'] + [f'col_{k}' for k in range(0, 2 * (total - 1))]
+    df = df.withColumn('clearing', my_udf(*cols))
+    new_cols = cols + ['clearing.linear', 'clearing.non_linear']
+
+    # Write the results of the experiments.
+    df = df.select(*new_cols)
+    df.write.mode('overwrite').parquet(sm_market_output_parquet_loc)
+    df.printSchema()
+
+    # Optionally, show some of the data frame, just for debugging purposes.
+    # df.show(n=1000)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 1 and len(sys.argv) != 4:
+        raise Exception("Either 0 or 3 command line arguments are accepted")
+    # Run the experiment with a default total of a total given by the user's parameter.
+    if len(sys.argv) == 1:
+        the_total = 7
+        the_input_path = 'all_sm_markets/values_1_to_10/'
+        the_output_path = 'experiments_results/'
+    else:
+        the_total = int(sys.argv[1])
+        the_input_path = sys.argv[2]
+        the_output_path = sys.argv[3]
+    print(f"Run experiment with total = {the_total}, input_path = {the_input_path}, output_path = {the_output_path}")
+    run_experiment(total=the_total,
+                   input_path=the_input_path,
+                   output_path=the_output_path)
