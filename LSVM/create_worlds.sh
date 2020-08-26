cba06b6c6ca4845fd39f7f2f3db084de8cbeb9be
eareyan <enrique3@gmail.com>
2020-08-26 13:06:22 -0400

    deleting LSVM folder

--- LSVM/create_worlds.sh
+++
@@ -1,7 +0,0 @@
-# Create 100 LSVM worlds - No Global bidders.
-a=0
-while [ $a -lt 100 ]; do
-  # $1 should contain the path to noisy_combinatorial_markets.jar.
-  java -jar "$1" worlds/world$a.json
-  a=$(expr $a + 1)
-done
